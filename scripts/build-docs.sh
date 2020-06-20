0982789079efee1fce8c4d0cba62146154aeae6c
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2020-06-21 01:38:29 +0300

    NinjaAPI - Fast Django REST framework

---
+++ scripts/build-docs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+set -e
+# Install pip
+cd /tmp
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.6 get-pip.py --user
+cd -
+
+# Install deps
+python3.6 -m pip install --user -r requirements.txt
+
+# build docs
+cd docs
+python3.6 -m mkdocs build
