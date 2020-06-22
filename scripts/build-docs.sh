d5feb4df50b84685100f41e8e59e513b742b5c27
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2020-06-22 11:51:38 +0300

    build docs script

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -x
 set -e
-# Install pip
-cd /tmp
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.6 get-pip.py --user
-cd -
 
-# Install deps
-python3.6 -m pip install --user -r requirements.dev.txt
+pip install mkdocs==1.1.2 mkdocs-material==5.1.7 markdown-include==0.5.1
 
-# build docs
 cd docs
-python3.6 -m mkdocs build
+mkdocs build
+
