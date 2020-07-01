768d512e0118d6f174d133fa8424d11947f05261
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2020-07-01 21:51:19 +0300

    version bump

--- ninja/__init__.py
+++ ninja/__init__.py
@@ -1,6 +1,6 @@
 """NinjaAPI - Fast Django REST framework"""
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 from ninja.main import NinjaAPI
 from ninja.params import Query, Path, Header, Cookie, Body, Form
