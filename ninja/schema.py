675bc5627e0cc8056fa275144dac9a36b45fc21f
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2020-08-11 18:40:16 +0300

    typo fix

--- ninja/schema.py
+++ ninja/schema.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, validator
 
 
 # Since "Model" word would be very confusing
-# when used in django contetext
+# when used in django context
 # this module basicaly makes alias for it named Schema
 
 
