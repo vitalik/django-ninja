2ce37aa5b93bc916a7126d1e9264b44ee971e862
Dmitriy Plaksunov <fojetin@gmail.com>
2021-01-15 18:21:47 +0300

    added test

--- tests/test_inheritance_routers.py
+++ tests/test_inheritance_routers.py
@@ -2,8 +2,6 @@ from ninja import NinjaAPI, Router
 import pytest
 from client import NinjaClient
 
-second_router = Router()
-first_router = Router()
 api = NinjaAPI()
 
 
@@ -12,17 +10,26 @@ def global_op(request):
     return "global"
 
 
+first_router = Router()
 @first_router.get("/endpoint")
 def router_op(request):
     return "first"
 
 
-@second_router.get("/endpoint")
+second_router_one = Router()
+@second_router_one.get("endpoint_1")
 def router_op(request):
-    return "second"
+    return "second 1"
 
 
-first_router.add_router("/second", second_router)
+second_router_two = Router()
+@second_router_two.get("endpoint_2")
+def router_op(request):
+    return "second 2"
+
+
+first_router.add_router("/second", second_router_one)
+first_router.add_router("/second", second_router_two)
 api.add_router("/first", first_router)
 
 
@@ -34,7 +41,8 @@ client = NinjaClient(api)
     [
         ("/endpoint", 200, "global"),
         ("/first/endpoint", 200, "first"),
-        ("/first/second/endpoint", 200, "second"),
+        ("/first/second/endpoint_1", 200, "second 1"),
+        ("/first/second/endpoint_2", 200, "second 2"),
     ],
 )
 def test_inheritance_responses(path, expected_status, expected_response):
