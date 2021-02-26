74624da4303973f271d38b148fc70f0c5253871b
Wim Olivier <wim.olivier@vcontractor.co.za>
2021-02-26 20:06:30 +0200

    Just make some code a bit nicer

--- docs/src/tutorial/query/code01.py
+++ docs/src/tutorial/query/code01.py
@@ -3,4 +3,4 @@ weapons = ["Ninjato", "Shuriken", "Katana", "Kama", "Kunai", "Naginata", "Yari"]
 
 @api.get("/weapons")
 def list_weapons(request, limit: int = 10, offset: int = 0):
-    return weapons[offset : offset + limit]
+    return weapons[offset: offset + limit]
