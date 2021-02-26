0f1e8af06586644c35504bf7a4244cc91d1c0bc0
Wim Olivier <wim.olivier@vcontractor.co.za>
2021-02-26 20:06:30 +0200

    Just make some code a bit nicer

--- docs/src/tutorial/query/code02.py
+++ docs/src/tutorial/query/code02.py
@@ -5,4 +5,4 @@ weapons = ["Ninjato", "Shuriken", "Katana", "Kama", "Kunai", "Naginata", "Yari"]
 def search_weapons(request, q: str, offset: int = 0):
     results = [w for w in weapons if q in w.lower()]
     print(q, results)
-    return results[offset : offset + 10]
+    return results[offset: offset + 10]
