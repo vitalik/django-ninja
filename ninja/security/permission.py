1b9cba81dba332f30249d79771f0091483a4432e
Maximilian Wassink <wassink.maximilian@protonmail.com>
2021-08-14 10:58:30 +0200

    make `BasePermission` callable

--- ninja/security/permission.py
+++ ninja/security/permission.py
@@ -5,6 +5,12 @@ from django.http import HttpRequest
 
 
 class BasePermission(ABC):
+    def __init__(self, permission: str) -> None:
+        self.permission = permission
+
+    def __call__(self, request: HttpRequest) -> Optional[Any]:
+        return self.has_permission(request, self.permission)
+
     @abstractmethod
     def has_permission(self, request: HttpRequest, permission: str) -> Optional[Any]:
         pass  # pragma: no cover
