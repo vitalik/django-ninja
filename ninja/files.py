d056d0e140e05953aaf496aa268e65e642ce3b73
Charlie Wilson <charlie.wilson4@gmail.com>
2022-03-22 09:06:18 +1100

    Add missing return value type hint

--- ninja/files.py
+++ ninja/files.py
@@ -18,5 +18,5 @@ class UploadedFile(DjangoUploadedFile):
         return v
 
     @classmethod
-    def __modify_schema__(cls, field_schema: Dict[str, Any], field: Optional[ModelField]):
+    def __modify_schema__(cls, field_schema: Dict[str, Any], field: Optional[ModelField]) -> None:
         field_schema.update(type="string", format="binary")
