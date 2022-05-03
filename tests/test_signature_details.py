1b11fd7d4483a3a6240f6db00dc056fde8a85e9d
Petter Friberg <petter_friberg@hotmail.com>
2022-05-03 18:24:46 +0200

    fixup! Handle class instances in `signature.details.is_collection_type`

--- tests/test_signature_details.py
+++ tests/test_signature_details.py
@@ -1,4 +1,5 @@
 import typing
+from sys import version_info
 
 import pytest
 
@@ -9,13 +10,10 @@ from ninja.signature.details import is_collection_type
     ("annotation", "expected"),
     [
         pytest.param(typing.List, True, id="true_for_typing_List"),
-        pytest.param(list[int], True, id="true_for_parameterized_native_list"),
         pytest.param(list, True, id="true_for_native_list"),
         pytest.param(typing.Set, True, id="true_for_typing_Set"),
-        pytest.param(set[int], True, id="true_for_parameterized_native_set"),
         pytest.param(set, True, id="true_for_native_set"),
         pytest.param(typing.Tuple, True, id="true_for_typing_Tuple"),
-        pytest.param(tuple[int], True, id="true_for_parameterized_native_tuple"),
         pytest.param(tuple, True, id="true_for_native_tuple"),
         pytest.param(
             type("Custom", (), {}),
@@ -23,9 +21,26 @@ from ninja.signature.details import is_collection_type
             id="false_for_custom_type_without_typing_origin",
         ),
         pytest.param(
+            object(), False, id="false_for_custom_instance_without_typing_origin"
+        ),
+        pytest.param(
             typing.NewType("SomethingNew", str),
             False,
             id="false_for_instance_without_typing_origin",
+        ),
+        # Can't mark with `pytest.mark.skipif` since we'd attempt to instantiate the
+        # parameterized value/type(e.g. `list[int]`). Which only works with Python >= 3.9)
+        *(
+            (
+                pytest.param(list[int], True, id="true_for_parameterized_native_list"),
+                pytest.param(set[int], True, id="true_for_parameterized_native_set"),
+                pytest.param(
+                    tuple[int], True, id="true_for_parameterized_native_tuple"
+                ),
+            )
+            # TODO: Remove conditional once support for <=3.8 is dropped
+            if version_info >= (3, 9)
+            else ()
         ),
     ],
 )
