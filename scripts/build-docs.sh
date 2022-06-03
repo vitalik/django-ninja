92f6b9c1c5543e140b5197b715282e179e122927
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2022-06-03 12:48:35 +0300

    fixed docs build

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-pip install mkdocs==1.1.2 mkdocs-material==5.1.7 markdown-include==0.5.1
+pip install mkdocs==1.1.2 mkdocs-material==5.1.7 markdown-include==0.5.1 jinja2==3.0.3
 
 cd docs
 mkdocs build
