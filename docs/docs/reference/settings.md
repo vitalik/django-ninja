2d9d3b4277c9ca2869938f861f194267cbc3c9c4
Chris Beaven <smileychris@gmail.com>
2022-07-19 20:46:13 +1200

    Document all the django-ninja django settings (rather than just the redoc one)

---
+++ docs/docs/reference/settings.md
@@ -0,0 +1,6 @@
+# Django Settings
+
+::: ninja.conf.Settings
+    rendering:
+      show_source: False
+      show_root_toc_entry: False
--- ninja/conf.py
+++ ninja/conf.py
@@ -3,6 +3,22 @@ from pydantic import BaseModel, Field
 
 
 class Settings(BaseModel):
+    """
+    Alter these by modifying the values in Django's settings module (usually
+    `settings.py`).
+
+    Attributes:
+        NINJA_PAGINATION_CLASS (str):
+            The pagination class to use. Defaults to
+            `ninja.pagination.Pagination`.
+        NINJA_PAGINATION_PAGE_SIZE (int):
+            The default page size. Defaults to `100`.
+        NINJA_DOCS_VIEW ("swagger"|"redoc"):
+            The view to use for the documentation. Defaults to `swagger`, but
+            change to `redoc` to use alternative
+            [Redoc](https://github.com/Redocly/redoc) automatic documentation.
+    """
+
     PAGINATION_CLASS: str = Field(
         "ninja.pagination.LimitOffsetPagination", alias="NINJA_PAGINATION_CLASS"
     )
