cbfd590887a861356188ebfa463b8daf98e5dcbf
msilva <msilva@synopsys.com>
2022-09-24 16:36:52 +0100

    #573 Add tests for OpenAPI Server support
    Add test for ServerObject
    Add test for ServerVariable
    Runs formatters and linters

--- ninja/server.py
+++ ninja/server.py
@@ -1,16 +1,18 @@
-from typing import Optional, List
+from typing import List, Optional
+
 from ninja.types import DictStrAny
 
 __all__ = ["Server", "ServerVariable"]
 
 
 class ServerVariable:
-    def __init__(self,
-                 variable: str,
-                 default: str,
-                 enum: Optional[List[str]] = None,
-                 description: Optional[str] = None,
-                 ) -> None:
+    def __init__(
+        self,
+        variable: str,
+        default: str,
+        enum: Optional[List[str]] = None,
+        description: Optional[str] = None,
+    ) -> None:
         self.variable: str = variable
         self.default: str = default
         self.enum: Optional[List[str]] = enum
@@ -27,11 +29,12 @@ class ServerVariable:
 
 
 class Server:
-    def __init__(self,
-                 url: str = '/',
-                 description: Optional[str] = None,
-                 variables: Optional[List[ServerVariable]] = None,
-                 ) -> None:
+    def __init__(
+        self,
+        url: str = "/",
+        description: Optional[str] = None,
+        variables: Optional[List[ServerVariable]] = None,
+    ) -> None:
         self.description = description
         self.url = url
         self.variables = variables
@@ -41,6 +44,8 @@ class Server:
         if self.description:
             result["description"] = self.description
         if self.variables:
-            result["variables"] = {variable.variable: variable.to_dict() for variable in self.variables}
+            result["variables"] = {
+                variable.variable: variable.to_dict() for variable in self.variables
+            }
 
         return result
