20c9668b38c97c64e994d2530791f83f3ee683ea
aasiffaizal <aasif.faizal@perpetua.io>
2023-01-30 10:47:55 -0500

    lint

--- tests/test_auth_inheritance_routers.py
+++ tests/test_auth_inheritance_routers.py
@@ -1,8 +1,8 @@
 import pytest
-from ninja.testing import TestClient
 
 from ninja import NinjaAPI, Router
 from ninja.security import APIKeyQuery
+from ninja.testing import TestClient
 
 
 class Auth(APIKeyQuery):
