bbabc58f821dba1986834ba9a3f41de6159b909f
Amine Zyad <amizya@gmail.com>
2023-11-11 12:04:37 +0100

    Fixed a typo

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ sys.stderr.write(
 ===============================
 Unsupported installation method
 ===============================
-django-ninja does not installation with `python setup.py install`.
-Please use `python -m pip install .` instead.
+django-ninja does not install with `python setup.py install`.
+Please use `python -m pip install` instead.
 """
 )
 sys.exit(1)
