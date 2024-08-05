5cda5391db9853e3d0b2e8aff8dd2e4f8bc55572
Remy <lapin.vert@gmail.com>
2024-08-05 19:22:46 +1000

    Fix no URL validation for ModelSchema URLFields Fixes #1157

--- ninja/orm/fields.py
+++ ninja/orm/fields.py
@@ -3,9 +3,9 @@ from decimal import Decimal
 from typing import Any, Callable, Dict, List, Tuple, Type, TypeVar, Union, no_type_check
 from uuid import UUID
 
-from django.db.models import ManyToManyField
+from django.db.models import ManyToManyField, URLField
 from django.db.models.fields import Field as DjangoField
-from pydantic import IPvAnyAddress
+from pydantic import AnyUrl, IPvAnyAddress
 from pydantic.fields import FieldInfo
 from pydantic_core import PydanticUndefined, core_schema
 
@@ -69,6 +69,7 @@ TYPES = {
     "SmallIntegerField": int,
     "TextField": str,
     "TimeField": datetime.time,
+    "URLField": AnyUrl,
     "UUIDField": UUID,
     # postgres fields:
     "ArrayField": List,
@@ -148,6 +149,9 @@ def get_schema_field(
         max_length = field_options.get("max_length")
 
         internal_type = field.get_internal_type()
+        if isinstance(field, URLField):
+            internal_type = "URLField"
+
         python_type = TYPES[internal_type]
 
         if field.primary_key or blank or null or optional:
--- tests/test_orm_schemas.py
+++ tests/test_orm_schemas.py
@@ -143,7 +143,7 @@ def test_all_fields():
             "smallintegerfield": {"type": "integer", "title": "Smallintegerfield"},
             "textfield": {"type": "string", "title": "Textfield"},
             "timefield": {"type": "string", "format": "time", "title": "Timefield"},
-            "urlfield": {"type": "string", "title": "Urlfield"},
+            "urlfield": {'format': 'uri', 'minLength': 1, 'title': 'Urlfield', 'type': 'string'},
             "uuidfield": {"type": "string", "format": "uuid", "title": "Uuidfield"},
             "arrayfield": {"type": "array", "items": {}, "title": "Arrayfield"},
             "cicharfield": {"type": "string", "title": "Cicharfield"},
