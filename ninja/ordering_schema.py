ab7ed55df5f813ef73ab6edba32edfdba1ea3da5
Aryan Curiel <aryan.curiel@gmail.com>
2024-09-02 16:40:01 +0300

    Added field validation to the OrderingBaseSchema
    Verifies if the provided values are part of the allowed fields

--- ninja/ordering_schema.py
+++ ninja/ordering_schema.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Generic, List, TypeVar
 
+from pydantic import ValidationError, field_validator
+
 from .schema import Schema
 
 T = TypeVar("T")
@@ -11,6 +13,19 @@ class OrderingBaseSchema(Schema, ABC, Generic[T]):
 
     class Config:
         allowed_fields = "__all__"
+
+    @field_validator("order_by")
+    @classmethod
+    def validate_order_by_field(cls, value: List[str]) -> List[str]:
+        allowed_fields = cls.Config.allowed_fields
+        if value and allowed_fields != "__all__":
+            allowed_fields_set = set(allowed_fields)
+            for order_field in value:
+                field_name = order_field.lstrip("-")
+                if field_name not in allowed_fields_set:
+                    raise ValidationError(f"Ordering by {field_name} is not allowed")
+
+        return value
 
     @abstractmethod
     def sort(self, elements: T) -> T:
