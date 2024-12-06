f9fa9cf7d25c7349a0764cf7240f6d0628219c65
Vitaliy Kucheryaviy <ppr.vitaly@gmail.com>
2024-12-06 18:53:07 +0200

    register_field utility

--- ninja/orm/__init__.py
+++ ninja/orm/__init__.py
@@ -1,4 +1,5 @@
 from ninja.orm.factory import create_schema
+from ninja.orm.fields import register_field
 from ninja.orm.metaclass import ModelSchema
 
-__all__ = ["create_schema", "ModelSchema"]
+__all__ = ["create_schema", "register_field", "ModelSchema"]
