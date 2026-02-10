fd135eabbc69a7c39ce0c5697be837040df6dffd
Ksauder <ksauder.dev@gmail.com>
2026-02-10 13:19:07 -0500

    safely use MISSING in schema.py

--- ninja/schema.py
+++ ninja/schema.py
@@ -35,13 +35,17 @@ from django.db.models.fields.files import FieldFile
 from django.template import Variable, VariableDoesNotExist
 from pydantic import BaseModel, ConfigDict, Field, ValidationInfo, model_validator
 from pydantic._internal._model_construction import ModelMetaclass
-from pydantic.experimental.missing_sentinel import MISSING
 from pydantic.functional_validators import ModelWrapValidatorHandler
 from pydantic.json_schema import GenerateJsonSchema, JsonSchemaValue
 from typing_extensions import dataclass_transform
 
 from ninja.signature.utils import get_args_names, has_kwargs
 from ninja.types import DictStrAny
+
+try:
+    from pydantic.experimental.missing_sentinel import MISSING
+except ImportError:
+    MISSING = object()
 
 pydantic_version = list(map(int, pydantic.VERSION.split(".")[:2]))
 assert pydantic_version >= [2, 0], "Pydantic 2.0+ required"
